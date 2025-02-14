12a5681ac2ce3ab24a52cefefb38e23d367b001a
Karan <karandayani39@gmail.com>
2025-02-14 19:03:14 +0530

    header update

---
+++ app/components/Header.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import { IoMenu } from "react-icons/io5";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className="font-semibold px-6 h-12">
+      <div className="flex justify-between items-center h-full">
+        <h1 className="text-xl">Karan</h1>
+        <IoMenu
+          className="h-5 w-5 md:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0, opacity: 0 }}
+              transition={{ duration: 0.5, type: "spring" }}
+              className="absolute top-10 right-6 w-36 max-h-48 rounded-md bg-[#F5F5F5] p-6 z-10 flex flex-col origin-top-right"
+            >
+              <nav className="flex flex-col text-black gap-2">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Work</a>
+                <a href="#">Contact</a>
+              </nav>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <nav className="gap-6 text-slate-400 hidden md:flex">
+          <a className="hover:text-black cursor-pointer transition-all duration-300">
+            Home
+          </a>
+          <a className="hover:text-black cursor-pointer transition-all duration-300">
+            About
+          </a>
+          <a className="hover:text-black cursor-pointer transition-all duration-300">
+            Work
+          </a>
+          <a className="hover:text-black cursor-pointer transition-all duration-300">
+            Contact
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+// import { motion } from "motion/react";
+// import photo from "../public/aied2.png";
+// import Image from "next/image";
+
 export default function Home() {
-  return <div>Hello Everynian!</div>;
+  return (
+    <>
+      <div>Home page</div>
+      {/* <div className="px-6">
+        <div className="h-96 flex gap-6">
+          <div className="w-full rounded-lg bg-gradient-to-r from-slate-200 to-[#492b27c9] p-10">
+            <h1 className="font-semibold text-4xl tracking-wider leading-relaxed">
+              Hii, I&#39;m Karan Dayani a full-stack developer &#40;Student&#41;
+              from India.
+            </h1>
+            <p className="mt-2">
+              I am an aspiring full-stack developer with experience in React,
+              Next.js, Node.js, and databases like MongoDB and PostgreSQL.
+              Passionate about building scalable web and mobile applications, I
+              enjoy solving real-world problems through code. Constantly
+              learning and exploring new technologies, my goal is to develop
+              efficient, user-friendly solutions.
+            </p>
+          </div>
+          <div className="bg-slate-400 w-2/5 rounded-lg">
+            <Image
+              src={photo}
+              alt="photo"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
 }
